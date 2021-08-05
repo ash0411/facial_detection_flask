44e3646585b67a2c522f5d5c0ef5095ded8aa4f0
ash0411 <aashishbora36@gmail.com>
2021-08-06 00:11:38 +0530

    result checker assigning url dynamically

--- MyPythonApp/app.py
+++ MyPythonApp/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 app = Flask(__name__)
 @app.route('/')
 def welcome():
-    return 'Welcome to the practice session. lets practice hard. okay
-@app.route('/members')
-def members():
-    return 'Welcome to the practice session.'
+    return 'Welcome to the practice session. lets practice hard. okay'
+@app.route('/success/<int:score>') # passing score as int (default string) called variable rules
+def success(score):
+    return 'The practice session has passed with '+str(score)+"%"
+@app.route('/fail/<int:score>')
+def fail(score):
+    fail_perc= 100 * (40-score)/40
+    return " the practice session has failed by "+str(fail_perc) + '%'
+# result checker
+@app.route('/results/<int:score>')
+def result(score):
+     res = 'success' if score > 40 else 'fail'
+     return redirect(url_for(res,score = score)) # for redirecting to other pages
 if __name__ == '__main__':
     app.run(debug = True) 
