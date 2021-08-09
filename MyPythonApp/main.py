f081e2995a67a9dae17b4f0619c0b05dc9a3c66a
ash0411 <aashishbora36@gmail.com>
2021-08-09 22:19:20 +0530

    using template to display result of all subjects

--- MyPythonApp/main.py
+++ MyPythonApp/main.py
@@ -1,17 +1,21 @@
 # integrating html with flask app
 ## using http verb get and post methods
+#Jinja2 template
+'''
+{%...%} any kind of statement if or for
+{{    }} expression to print output
+{#....#} this is for comments
+'''
 from flask import Flask,redirect,url_for,render_template,request
+import json
 app = Flask(__name__)
 @app.route('/')
 def welcome():
     return render_template('index.html')
-@app.route('/success/<int:score>') # passing score as int (default string) called variable rules
+@app.route('/success/<string:score>') # passing score as int (default string) called variable rules
 def success(score):
-    if score >=50:
-        res = 'PASSED'
-    else:
-        res = 'FAILED'
-    return render_template('result.html',result = res)
+    score = json.loads(score)
+    return render_template('result.html',result = score)
 @app.route('/fail/<int:score>')
 def fail(score):
     fail_perc= 100 * (40-score)/40
@@ -25,12 +29,15 @@ def result(score):
 def submit():
     total_score = 0
     if request.method == 'POST':
-        science = float(request.form['science']) # the name of subject
-        computer_science = float(request.form['computer science']) # the name of subject
-        maths = float(request.form['maths']) # the name of subject
-        c = float(request.form['c']) # the name of subject
-        python = float(request.form['python']) # the name of subject
-        total_score = (science + computer_science + maths + c + python)/5
-    return redirect(url_for('success',score=total_score))
+        res = {}
+        res['science'] = float(request.form['science']) # the name of subject
+        res['computer_science'] = float(request.form['computer science']) # the name of subject
+        res['maths'] = float(request.form['maths']) # the name of subject
+        res['c'] = float(request.form['c']) # the name of subject
+        res['python'] = float(request.form['python']) # the name of subject
+        print(type(res))
+        res['whole exam'] = (res['science'] + res['computer_science'] + res['maths'] + res['c'] + res['python'])/5
+        inp = json.dumps(res)
+    return redirect(url_for('success',score=inp))
 if __name__ == '__main__':
     app.run(debug = True)  
