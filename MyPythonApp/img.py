bf2cbe722b92f34aaa1e8946b0688fd0bc94dbf6
ash0411 <aashishbora36@gmail.com>
2021-08-12 01:41:33 +0530

    added face and eye detection

--- MyPythonApp/img.py
+++ MyPythonApp/img.py
@@ -5,15 +5,18 @@ camera = cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
-        # read the camera frame 
-        success,frame = camera.read()
+            
+        ## read the camera frame
+        success,frame=camera.read()
         if not success:
             break
         else:
-            ret,buffer = cv2.imencode('.jpg',frame)
-            frame = buffer.tobytes()
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
         yield(b'--frame\r\n'
-                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
  
 @app.route('/')
 def index():
