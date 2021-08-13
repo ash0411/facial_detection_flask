47dc9ebe1ec91689036e4d985f5a5b057ae75f7c
ash0411 <aashishbora36@gmail.com>
2021-08-13 20:52:27 +0530

    added image capture and start/stop

--- MyPythonApp/face_detection.py
+++ MyPythonApp/face_detection.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template,Response
+from flask import Flask, render_template,Response,request
 import cv2
+import datetime, time
+import os,sys
+import numpy as np
+from    threading import Thread
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+# making shot directory to save files
+try:
+    os.mkdir('shots')
+except OSError as error:
+    print('Not able to make a directory')
+    pass
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
@@ -33,6 +50,50 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/tasks',methods=['GET','POST'])
+def tasks():
+    global switch, camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'capture':
+            global capture
+            capture = 1
+        elif request.form.get('grey') == 'grey':
+            global grey
+            grey = 1
+        elif request.form.get('negative')=='negative':
+            global neg
+            neg = not neg
+        elif request.form.get('face_only') == 'face_only':
+            global face
+            face = not face
+            if(face):
+                time.sleep(4)
+        elif request.form.get('stop') == 'stop/start':
+            if switch == 1:
+                switch = 0
+                camera.release()
+                cv2.destroyAllWindows()
+            else:
+                camera = cv2.VideoCapture(0)
+                switch = 1
+        elif request.form.get('rec') == 'start/stop recording':
+            global rec, out
+            rec = not rec
+            # if(rec):
+            #     now=datetime.datetime.now() 
+            #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            #     out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+            #     #Start new thread for recording the video
+            #     thread = Thread(target = record, args=[out,])
+            #     thread.start()
+            # elif(rec==False):
+            #     out.release()
+    elif request.method == 'GET':
+        return render_template('index_img.html')
+    return render_template('index_img.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
